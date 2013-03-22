a013238b64d4f1a6e56e94a67f6f67c5137c3a56
straup <straup@gmail.com>
2013-03-21 20:46:48 -0400

    start making export2files do stuff

--- bin/export2files.py
+++ bin/export2files.py
@@ -9,5 +9,56 @@ import logging
 
 if __name__ == '__main__':
 
-    print "please write me"
+    import optparse
+
+    parser = optparse.OptionParser("""export.py --options""")
+
+    parser.add_option("-e", "--export", dest="export", help="...", default=None)
+    parser.add_option("-o", "--outdir", dest="outdir", help="...", default=None)
+    parser.add_option("-p", "--placetype", dest="placetype", help="...", default=None)
+    parser.add_option("-v", "--verbose", dest="verbose", action="store_true", help="enable chatty logging; default is false", default=False)
+
+    (opts, args) = parser.parse_args()
+
+    if opts.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    placetype = opts.placetype
+    fname = "%s-poly.json" % placetype
+
+    path = os.path.join(opts.export, placetype, fname)
+
+    if not os.path.exists(path):
+        logging.error("%s is not a file!" % path)
+        sys.exit()
+
+    fh = open(path, 'r')
+    data = json.load(fh)
+
+    for f in data['features']:
+
+        geojson = {
+            'type': 'FeatureCollection',
+            'features': [ f ]
+        }
+
+        woeid = f['id']
+
+        tree = utils.woeid2path(woeid)
+        fname = "%s.json" % woeid
+
+        root = os.path.join(opts.outdir, tree)
+        path = os.path.join(root, fname)
+
+        if not os.path.exists(root):
+            logging.info("create %s" % root)
+            os.makedirs(root)
+
+        logging.info("write %s" % path)
+
+        out = open(path, 'w')
+        utils.write_json(geojson, out)
+
     sys.exit()
